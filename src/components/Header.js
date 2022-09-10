3b097e917f9bc2a8360a75ccff808f7f81a3925e
Sean Watson <sean_watson30@me.com>
2022-09-10 12:07:24 -0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import data from "../data/data.js";
+
+function Header() {
+  console.log(data)
+  return (
+    <h1>Header</h1>
+  )
+}
+
+export default Header
---
+++ src/data/data.js
@@ -0,0 +1,13 @@
+const data = [
+  {
+    title: 'test',
+    location: '',
+    googleMapsUrl: '',
+    startDate: '',
+    endDate: '',
+    description: '',
+    imageUrl: '',
+  }
+]
+
+export default data
