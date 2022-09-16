3472cb13b1336d429d112a2e2a732fe8a103b1d9
Sean Watson <sean_watson30@me.com>
2022-09-15 22:34:09 -0600

    cleans up notes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ function Header() {
     <header className="header">
         <i className="bi bi-globe2" id="header-content"></i>
         <h1 id="header-content">My Travel Journal</h1>
-      {/* <img src="" alt="" /> */}
     </header>
   )
 }
